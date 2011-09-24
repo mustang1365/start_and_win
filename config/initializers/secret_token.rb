d926dc493cb50b8d9ce67a41cafd7afc141a9504
Zhenya Kostev <m3.developers@gmail.com>
2011-09-24 20:20:17 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartAndWin::Application.config.secret_token = '48e1a5b22a723d53b9d8deef36c724d74dd8910fc09a1a865fcb483e6a6bae4296389eb2665fbe81b477294396e29e1ddb41a8c58c4255235aa5eb782d4da4ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StartAndWin::Application.config.session_store :cookie_store, key: '_start_and_win_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StartAndWin::Application.config.session_store :active_record_store
