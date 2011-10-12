2c96d2d58f571aff2bdeeb523041f583b1ae695a
Zhenya Kostev <m3.developers@gmail.com>
2011-10-13 00:14:37 +0300

    [User Registrations]  War with saving profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,20 @@
+#encoding: utf-8
 class ProfilesController < ApplicationController
   def show
-
+   @user_to_show = User.find(params[:id])
   end
 
   def edit
-    @user_to_edit = User.find(current_user.id)
+    @user_to_edit = current_user
   end
 
   def update
-
+    @user_to_edit = User.find(Profile.find(params[:id]).user.id)
+    if User.last.update_attributes(params[:user])
+      flash[:notice] = 'Профиль успешно обновлен'
+      redirect_to :action => :show, :id => @user_to_edit.id
+    else
+      render :action => :edit
+    end
   end
 end
