b182541881091888ebc09e49e81ad8cc2b7b2ca7
Zhenya Kostev <m3.developers@gmail.com>
2012-02-21 23:43:26 +0300

    News point hot fix

--- app/models/news_point.rb
+++ app/models/news_point.rb
@@ -3,8 +3,7 @@ class NewsPoint < ActiveRecord::Base
 
   validates :title, :presence => true
 
-
   def news_point_image_url
-    self.image.image_url.present? ? self.image.image_url : '/assets/pic1.png'
+    self.try(:image).try(:image_url).present? ? self.image.image_url : '/assets/pic1.png'
   end
 end
